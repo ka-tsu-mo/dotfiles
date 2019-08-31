12e2dce1460c64bd4c008a347ef9503121d56dae
ka-tsu-mo <katsumo.github@gmail.com>
2019-09-01 01:14:51 +0900

    update Brewfile, install.sh

--- install.sh
+++ install.sh
@@ -33,14 +33,81 @@ download_dotfiles() {
     if exists "git"; then
         git clone https://github.com/ka-tsu-mo/dotfiles.git "$DOTFILES_PATH"
     elif exists "curl"; then
+        curl -L https://github.com/ka-tsu-mo/dotfiles/archive/master.tar.gz | tar xz
+        mv dotfiles-master "$DOTFILES_PATH"
     else
         echo "git or curl is required"
+        exit 1
     fi
+}
+
+deploy() {
+    ln -s "$DOTFILES_PATH/zsh/.zshenv" ~/.zshenv
+}
+
+setup_homebrew() {
+    xcode-select --install
+    if [ "$?" = 1 ]; then
+        echo "failed to install command line tool"
+        exit 1
+    fi
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    if [ "$?" = 1 ]; then
+        echo "failed to install Homebrew"
+        exit 1
+    fi
+    brew update
+    if [ "$?" = 1 ]; then
+        echo "failed to update Homebrew"
+        exit 1
+    fi
+    brew doctor
+    if [ "$?" = 1 ]; then
+        echo "not ready to brew"
+        exit 1
+    fi
+}
+
+setup_zsh() {
+    brew install zsh
+    if [ "$?" = 1 ]; then
+        echo "failed to install zsh"
+        exit 1
+    fi
+    git clone --recursive https://github.com/sorin-ionescu/prezto.git "$DOTFILES_PATH/.zprezto"
+    echo /usr/local/bin/zsh >> /etc/shells
+    chsh -s /usr/local/bin/zsh
+    source ~/.zshenv
+}
+
+setup_nvim() {
+    gem install neovim
+    pip2 install neovim
+    pip3 install neovim
+    npm install neovim
+    curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ~/installer.sh
+    sh ~/installer.sh ~/.cache/dein
+    rm ~/installer.sh
+}
+
+initialize() {
+    if exists "brew"; then
+        echo "Homebrew is already installed"
+    else
+        setup_homebrew
+    fi
+    brew install git
+    setup_zsh
+    cd "$DOTFILES_PATH"
+    brew bundle
+    setup_nvim
 }
 
 main() {
     check_os
     download_dotfiles
+    deploy
+    initialize
 }
 
 main

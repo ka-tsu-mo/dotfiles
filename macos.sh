2531b60ddc66cd62d00794a80b215d9f6116e55d
ka-tsu-mo <katsumo.github@gmail.com>
2019-09-01 17:44:41 +0900

    add macos.sh

---
+++ macos.sh
@@ -0,0 +1,11 @@
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write com.apple.menuextra.battery ShowPercent -string YES
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock orientation -string left
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.dock magnification -bool yes
+defaults write com.apple.dock tilesize -int 35
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write NSGlobalDomain _HIHideMenuBar -bool true
